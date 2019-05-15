a36618a6cb038861aba4c9eeba653a4bfed38534
niss36 <nissim.chekroun@gmail.com>
2019-05-15 14:41:06 +0100

    Fixed imports of WeeklyReportController

--- src/uk/ac/cam/gr3/weather/gui/controllers/WeeklyReportController.java
+++ src/uk/ac/cam/gr3/weather/gui/controllers/WeeklyReportController.java
@@ -2,11 +2,12 @@ package uk.ac.cam.gr3.weather.gui.controllers;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import javax.swing.text.html.ImageView;
-import java.awt.*;
+import javafx.scene.paint.Color;
 
 public class WeeklyReportController {
 
@@ -20,27 +21,27 @@ public class WeeklyReportController {
         wednesday.setPrefHeight(50.0);
         wednesday.setPrefWidth(360.0);
 
-        javafx.scene.image.Image moodImage = new javafx.scene.image.Image(icon);
-        javafx.scene.image.ImageView mood = new javafx.scene.image.ImageView(moodImage);
+        Image moodImage = new Image(icon);
+        ImageView mood = new ImageView(moodImage);
         //<ImageView fitHeight="50.0" fitWidth="80.0" pickOnBounds="true" preserveRatio="true" />
         mood.setFitHeight(50.0);
         mood.setFitWidth(80.0);
         mood.setPickOnBounds(true);
         mood.setPreserveRatio(true);
-        javafx.scene.control.Label day = new javafx.scene.control.Label(dayName);
+        Label day = new Label(dayName);
         //<Label prefHeight="18.0" prefWidth="149.0" text="Tuesday" />
         day.setPrefHeight(18.0);
         day.setPrefWidth(149.0);
-        javafx.scene.control.Label tempMax = new javafx.scene.control.Label(tempHigh);
+        Label tempMax = new Label(tempHigh);
         //<Label prefHeight="18.0" prefWidth="43.0" text="5 °C">
         tempMax.setPrefHeight(18.0);
         tempMax.setPrefWidth(43.0);
-        tempMax.setTextFill(javafx.scene.paint.Color.web("#ff0000", 1.0));
-        javafx.scene.control.Label tempMin = new javafx.scene.control.Label(tempLow);
+        tempMax.setTextFill(Color.web("#ff0000", 1.0));
+        Label tempMin = new Label(tempLow);
         //<Label prefHeight="18.0" prefWidth="43.0" text="5 °C">
         tempMin.setPrefHeight(18.0);
         tempMin.setPrefWidth(43.0);
-        tempMin.setTextFill(javafx.scene.paint.Color.web("#0000ff", 1.0));
+        tempMin.setTextFill(Color.web("#0000ff", 1.0));
         wednesday.getChildren().add(mood);
         wednesday.getChildren().add(day);
         wednesday.getChildren().add(tempMax);
